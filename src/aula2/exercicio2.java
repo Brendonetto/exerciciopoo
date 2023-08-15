f6eee2e4e03487b0f3d46657799eff20fffa0d06
Brendonetto <brendonetto_2000@hotmail.com>
2023-08-15 20:45:37 -0300

    exercicio 2 da aula 3

---
+++ src/aula2/exercicio2.java
@@ -0,0 +1,75 @@
+package aula2;
+
+public class exercicio2
+{
+    public static void main(String[] args) {
+//EXERCICIO1
+
+//        char segunda = '2';
+//
+//        switch (segunda){
+//            case 1:
+//                System.out.println("Domingo");
+//            break;
+//            case 2 :
+//                System.out.println("Segunda");
+//                break;
+//            case 3:
+//            System.out.println("terça");
+//            break;
+//            case 4 :
+//                System.out.println("Quarta");
+//                break;
+//            case 5:
+//                System.out.println("Quinta");
+//                break;
+//            case 6 :
+//                System.out.println("Sexta");
+//                break;
+//            case 7:
+//                System.out.println("Sabado");
+//                break;
+//            default:
+//                System.out.println("?");
+//        }
+//    }
+//}
+//
+//
+///EXERCICIO2
+//
+//        int x = 10;
+//        while (x <= 30) {
+//        System.out.println("item " + x);
+//        x++;
+//        }
+//        }
+//    }
+//
+//        EXERCICIO3
+//        int y = 10;
+//        do {
+//            System.out.println("item " + y);
+//            y++;
+//        } while (y < 30);
+//    }
+//}
+//
+//        double h = 4.4567;
+//        double b = 3.6545;
+//        double B = 4;
+//
+//        double basemaior = (h.(b + B))/2;
+//
+//        System.out.println(basemaior);
+//    }
+//}
+//}
+//
+//
+//
+
+
+
+
+
