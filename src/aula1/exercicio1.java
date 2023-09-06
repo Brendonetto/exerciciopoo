d2951621bc3f00c2bde803dcbb99e0bb3d451db5
Brendonetto <brendonetto_2000@hotmail.com>
2023-09-05 21:38:11 -0300

    exercicio 3.1

---
+++ src/aula1/exercicio1.java
@@ -0,0 +1,13 @@
+package aula1;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        byte x = 50;
+        float maquina_precisao = 1.33676654f;
+        int cartao_credito = 549354;
+        char lota_do_aluno = 'o';
+        boolean existe = true;
+        double pacotes = 3.43439843943;
+
+    }
+}
